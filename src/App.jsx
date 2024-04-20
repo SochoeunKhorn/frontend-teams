1df85f77854ca497bcee15d2db2034251b5aebba
Ly Lihieng <heangly491@gmail.com>
2024-04-20 15:16:31 +0700

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {" "}
+      <div className="mr-32 ml-32 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8">
+        <div>
+          <div className="p-5 shadow-lg rounded cursor-pointer">
+            <img src="./src/assets/Cutelogo.jpg" alt="Logo" />
+          </div>
+
+          <h2 className="mt-4 mb-2 font-bold hover:text-green-400 flex justify-center items-center">
+            Our Programs
+          </h2>
+        </div>
+        <div>
+          <div className="p-5 shadow-lg rounded cursor-pointer">
+            <img src="./src/assets/Cutelogo.jpg" alt="Logo" />
+          </div>
+
+          <h2 className="mt-4 mb-2 font-bold hover:text-green-400 flex justify-center items-center">
+            Our Programs
+          </h2>
+        </div>
+        <div>
+          <div className="p-5 shadow-lg rounded cursor-pointer">
+            <img src="./src/assets/Cutelogo.jpg" alt="Logo" />
+          </div>
+
+          <h2 className="mt-4 mb-2 font-bold hover:text-green-400 flex justify-center items-center">
+            Our Programs
+          </h2>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
